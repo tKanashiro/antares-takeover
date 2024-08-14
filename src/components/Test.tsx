19427603dbc56c5c48ce40cb3503205e36064087
Anto Nellissery <anto.nellissery@aligent.com.au>
2024-08-14 16:18:20 +0930

    Temp fix for apollo client not loading in React components

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,31 +1,21 @@
-import React from "react";
-import pkg from "@apollo/client";
-const { ApolloProvider, useQuery, gql } = pkg;
-import { client } from "../lib/apollo-client";
+import { gql, useQuery } from "@apollo/client";
 
 const GET_COUNTRIES = gql`
-    query getCountries {
-        countries {
-            two_letter_abbreviation
-            full_name_english
-            available_regions {
-                code
-                name
-                id
-            }
-        }
+  query getCountries {
+    countries {
+      two_letter_abbreviation
+      full_name_english
+      available_regions {
+        code
+        name
+        id
+      }
     }
+  }
 `;
 
-const MyApp = () => {
-    // const { data } = useQuery(GET_COUNTRIES);
-    // if (data) console.log({ data });
-
-    return (
-        <ApolloProvider client={client}>
-            <div>Test file</div>
-        </ApolloProvider>
-    );
-};
-
-export default MyApp;
+export default function Test() {
+  const { data } = useQuery(GET_COUNTRIES);
+  if (data) console.log({ data });
+  return <p>{JSON.stringify(data)}</p>;
+}
