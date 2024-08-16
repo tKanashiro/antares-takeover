2de8de343c348cc5e8c503cb9c4069f57b7f8fc0
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2024-08-16 13:44:16 +0930

    Remove import

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,10 +1,8 @@
-// import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://take-flight-ew3k5nq-ekxw7lyelhava.ap-4.magentosite.cloud/graphql",
-  cache: new InMemoryCache(),
+    uri: "https://take-flight-ew3k5nq-ekxw7lyelhava.ap-4.magentosite.cloud/graphql",
+    cache: new InMemoryCache(),
 });
 
 export default client;
