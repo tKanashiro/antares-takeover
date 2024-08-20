059cf06da8b02448966c233757dc9d1203752824
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2024-08-20 13:11:51 +0930

    Apollo presentation - explanation

--- src/components/Apollo/ApolloMain.tsx
+++ src/components/Apollo/ApolloMain.tsx
@@ -1,19 +1,12 @@
-import { ApolloProvider } from "@apollo/client";
-import { client } from "../../lib/apollo-client";
-import Products from "./Products";
-
-/* For detailed error message in dev */
-// import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
-
-// loadDevMessages();
-// loadErrorMessages();
-/**************************************/
+import "./ApolloMain.css";
 
 const ApolloMain = () => {
     return (
-        <ApolloProvider client={client}>
-            <Products />
-        </ApolloProvider>
+        <>
+            <a className="demoLink" href="/apollo-cache">
+                Demo
+            </a>
+        </>
     );
 };
 
