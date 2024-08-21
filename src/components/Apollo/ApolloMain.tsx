476bfc702c614aa29ea327f0e94a83ec245697b8
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2024-08-21 10:34:55 +0930

    Add cache explanation images support

--- src/components/Apollo/ApolloMain.tsx
+++ src/components/Apollo/ApolloMain.tsx
@@ -1,12 +1,18 @@
 import "./ApolloMain.css";
+import requestImg from "./images/request.png";
+import responseImg from "./images/response.png";
+import splitImg from "./images/split.png";
 
 const ApolloMain = () => {
     return (
-        <>
+        <div className="body">
+            <img src={requestImg.src} alt="request" />
+            <img src={responseImg.src} alt="response" />
+            <img className="splitResponseImg" src={splitImg.src} alt="split" />
             <a className="demoLink" href="/apollo-cache">
                 Demo
             </a>
-        </>
+        </div>
     );
 };
 
